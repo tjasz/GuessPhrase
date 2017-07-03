acc4e266a686aa630d8d01b3472173779a5acb71
tjasz <nukorausa@hotmail.com>
2017-07-03 13:04:01 -0700

    Add Wikipedia class that queries for links, draft AddCategoryActivity to show one page's links.

--- app/src/main/java/com/example/tjasz/guessphrase/AddCategoryActivity.java
+++ app/src/main/java/com/example/tjasz/guessphrase/AddCategoryActivity.java
@@ -1,16 +1,39 @@
 package com.example.tjasz.guessphrase;
 
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.text.TextUtils;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 
 public class AddCategoryActivity extends ActionBarActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_category);
+        tv = (TextView) findViewById(R.id.add_category_textview);
+        new QueryWikipediaTask().execute("United%20States");
+    }
+
+    private class QueryWikipediaTask extends AsyncTask<String, Void, ArrayList<String>> {
+        @Override
+        protected void onPreExecute() {
+
+        }
+
+        @Override
+        protected ArrayList<String> doInBackground(String... params) {
+            return Wikipedia.getLinks(params[0]);
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> result) {
+            tv.setText(TextUtils.join("\n", result));
+        }
     }
 }
--- app/src/main/java/com/example/tjasz/guessphrase/Wikipedia.java
+++ app/src/main/java/com/example/tjasz/guessphrase/Wikipedia.java
@@ -1,7 +1,106 @@
 package com.example.tjasz.guessphrase;
 
-/**
- * Created by Tyler on 003 3 Jul 17.
- */
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 public class Wikipedia {
+    private static final String BASE_URI = "https://en.wikipedia.org/w/api.php?format=json";
+
+    public static ArrayList<String> getLinks(String pageName) {
+        boolean hasMore = true;
+        String nextPage = null;
+        ArrayList<String> linksList = new ArrayList<>();
+        try {
+            while (hasMore) {
+                // form query string
+                String qString = "&action=query&prop=links&pllimit=500&plnamespace=0&titles=" + pageName;
+                if (nextPage != null && nextPage.length() > 0) {
+                    qString += "&plcontinue=" + nextPage;
+                }
+                JSONObject qResult = new JSONObject(query(qString));
+                // get list of links from this query
+                JSONObject pages = qResult.getJSONObject("query").getJSONObject("pages");
+                Iterator<String> keys = pages.keys();
+                while (keys.hasNext()) {
+                    String keyValue = keys.next();
+                    JSONObject page = pages.getJSONObject(keyValue);
+                    JSONArray links = page.getJSONArray("links");
+                    for (int i = 0; i < links.length(); i++) {
+                        JSONObject link = links.getJSONObject(i);
+                        linksList.add(link.getString("title"));
+                    }
+                }
+                // continue query if necessary
+                if (qResult.has("continue")) {
+                    nextPage = qResult.getJSONObject("continue").getString("plcontinue");
+                } else {
+                    hasMore = false;
+                }
+            }
+        }
+        catch (JSONException e) {
+            throw new RuntimeException(e);
+        }
+        return linksList;
+    }
+
+    public static String query(String URI) {
+        String data = "";
+        HttpURLConnection huc = null;
+        try {
+            huc = (HttpURLConnection) new URL(BASE_URI + URI).openConnection();
+            InputStream in = new BufferedInputStream(huc.getInputStream());
+            data = readStream(in);
+        }
+        catch (MalformedURLException e) {
+            throw new RuntimeException(e);
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            if (huc != null) {
+                huc.disconnect();
+            }
+        }
+        return data;
+    }
+
+    private static String readStream(InputStream in) {
+        BufferedReader reader = null;
+        StringBuilder data = new StringBuilder("");
+        try {
+            reader = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                data.append(line);
+            }
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                }
+                catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return data.toString();
+    }
 }
