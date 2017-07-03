e013966c78f197d3602dfe5678980171bec45e77
tjasz <nukorausa@hotmail.com>
2017-07-02 23:21:53 -0700

    Load category names and buttons asynchronously, to avoid freezing up UI thread.

--- app/src/main/java/com/example/tjasz/guessphrase/SelectCategoryActivity.java
+++ app/src/main/java/com/example/tjasz/guessphrase/SelectCategoryActivity.java
@@ -1,38 +1,63 @@
 package com.example.tjasz.guessphrase;
 
+import android.app.Activity;
 import android.content.res.AssetManager;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 
 public class SelectCategoryActivity extends ActionBarActivity {
-
-    AssetManager am;
-    LinearLayout ll;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_category);
 
-        // dynamically add a Button for each Category available
-        am = getAssets();
-        ll = (LinearLayout) findViewById(R.id.categoryButtonsLayout);
-        try {
-            String[] assets = am.list("category");
-            for (int i = 0; i < assets.length; i++) {
-                Category cat = new Category();
-                cat.readJSONFile(am.open("category/" + assets[i]));
-                CategoryButton newButton = new CategoryButton(this, "category/" + assets[i]);
-                newButton.setText(cat.getName());
-                ll.addView(newButton);
-            }
+        new LoadCategoriesTask().execute(this);
+    }
+
+    private class LoadCategoriesTask extends AsyncTask<Activity, Void, ArrayList<CategoryButton>> {
+        Activity myActivity;
+
+        @Override
+        protected void onPreExecute() {
+
         }
-        catch (IOException e) {
-            throw new RuntimeException(e);
+
+        @Override
+        protected ArrayList<CategoryButton> doInBackground(Activity... params) {
+            // create a button for each category in the assets folder
+            myActivity = params[0];
+            ArrayList<CategoryButton> result = new ArrayList<>();
+            AssetManager am = myActivity.getAssets();
+            try {
+                String[] assets = am.list("category");
+                for (int i = 0; i < assets.length; i++) {
+                    Category cat = new Category();
+                    cat.readJSONFile(am.open("category/" + assets[i]));
+                    CategoryButton newButton = new CategoryButton(myActivity, "category/" + assets[i]);
+                    newButton.setText(cat.getName());
+                    result.add(newButton);
+                }
+            }
+            catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<CategoryButton> result) {
+            // add the category buttons to the LinearLayout in SelectCategoryActivity
+            LinearLayout ll = (LinearLayout) myActivity.findViewById(R.id.categoryButtonsLayout);
+            for (int i = 0; i < result.size(); i++) {
+                ll.addView(result.get(i));
+            }
         }
     }
 }
